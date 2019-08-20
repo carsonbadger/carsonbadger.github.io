542c9322c4eefb2b893680810341f640ab900602
macklan <macklanb@uw.edu>
2019-08-19 22:03:33 -0700

    Updates

---
+++ precache-manifest.b54dbf41f19ccd752e28bd4e056207ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c8897b9ea4124e90be8ead4d063d8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28fbaf23299c892dd35d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a484d462bd8f8c734fdb",
+    "url": "/static/js/2.56da3cac.chunk.js"
+  },
+  {
+    "revision": "28fbaf23299c892dd35d",
+    "url": "/static/js/main.09374bed.chunk.js"
+  },
+  {
+    "revision": "ea8f5a12ba7be1d6b380",
+    "url": "/static/js/runtime~main.43b06b8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
